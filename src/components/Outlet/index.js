64906e3b4208f450de4020046673dfe2692760b4
Amin Ghazi <aminghazi@outlook.com>
2023-04-17 14:51:07 +0330

    add useNavigate and useLocation from react-router-dom

--- src/components/Outlet/index.js
+++ src/components/Outlet/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import "./index.css"
 
 export default function OutletRoot() {
+
+  let navigate = useNavigate()
+
+
+  let location = useLocation()
+  // gets some properties from url (hash, key, pathname, search, ...)
+  console.log("location", location)
+
   return (
         <div className="Outlet-content">
           <h1>Outlet</h1>
+          <button onClick={() => {
+            navigate(-2)
+          }}>
+            back 2 navigate
+          </button>
+          <button onClick={() => {
+            // no save navigate to the browser => replace:true
+            navigate('/admin', {replace:true})
+          }}>
+            go to admin
+          </button>
           <ul className="Outlet-links">
               <li>
                   <Link to="outlet-root1">Root 1</Link>
